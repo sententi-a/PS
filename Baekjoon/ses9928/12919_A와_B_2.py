48e861fac6326a426391f1a0c9ce7df4c6f038a1
HyejiYeom <kristi8041@gmail.com>
2023-05-14 20:04:14 +0900

    [Python3][BruteForce]12919 A와 B 2

--- Baekjoon/ses9928/12919_A와_B_2.py
+++ Baekjoon/ses9928/12919_A와_B_2.py
@@ -12,57 +12,68 @@ from collections import Counter
 start = sys.stdin.readline().rstrip()
 goal = sys.stdin.readline().rstrip()
 
-examine = list(goal)
+# 1st 시도 : 반복문을 통해 A/B 하나씩 제거하면서 되돌아감 
+# examine = goal
 
-start_counter = Counter(start)
-goal_counter = Counter(goal)
+# start_counter = Counter(start)
+# goal_counter = Counter(goal)
 
-if goal_counter['A'] < start_counter['A'] or goal_counter['B'] < start_counter['B']:
-    print(0)
-    exit()
+# if goal_counter['A'] < start_counter['A'] or goal_counter['B'] < start_counter['B']:
+#     print(0)
+#     exit()
 
-add_counter = goal_counter - start_counter
+# add_counter = goal_counter - start_counter
 
-while len(start) != len(examine):
-    # A를 먼저 제거해야 하는 경우 / B를 제거해야 하는 경우 조건 생각하기 
-    if add_counter['A'] and examine[-1] == 'A':
-        examine.pop()
-        add_counter['A'] -= 1
+# while True:
+#     # print(examine)
 
-    # B가 추가되었다면 무조건 그 B는 맨 앞으로 갔음
-    elif add_counter['B'] and examine[0] == 'B':
-        examine = examine[1:]
-        examine = examine[::-1]
-        add_counter['B'] -= 1
+#     if len(examine) < len(start):
+#         print(0)
+#         exit() 
 
-    else:
-        print(0)
-        exit()
+#     if start == examine:
+#         print(1)
+#         exit() 
 
-if start == "".join(examine):
-    print(1)
+#     if add_counter['A'] and examine[-1] == 'A':
+#     # if examine[-1] == 'A':
+#         examine = examine[:-1]
+#         add_counter['A'] -= 1
 
-else:
-    print(0)
+#     # B가 추가되었다면 무조건 그 B는 맨 앞으로 갔음
+#     elif examine[0] == 'B':
+#         examine = examine[1:][::-1]
 
-# BAAAAABAA 
-# BAAAAABAA'B'
-# 'B'AABAAAAAB
+#     else:
+#         print(0)
+#         exit()
 
-# A
-# A'B'
-# 'B'A
-# BA'B'
-# 'B'AB
-# BABA
+# 2nd 시도 : backtracking 원리를 차용한 반복문 사용 
+stack = [goal]
+flag = False 
 
-# A
-# A'B'
-# 'B'A
-# BA'B'
-# 'B'AB
+while stack:
+    arr = stack.pop()
 
-# BA
-# BAA
-# BAAB
-# BBAAB
+    if len(arr) == len(start):
+        if arr == start:
+            flag = True
+            break
+        continue
+
+    if arr[-1] == 'A':
+        new_arr = arr[:-1]
+        stack.append(new_arr)
+
+    if arr[0] == 'B':
+        new_arr = arr[1:][::-1]
+        stack.append(new_arr)
+
+print(1 if flag else 0)
+
+# B를 없애야 할 때랑, A를 없애야 할 때가 있는데 둘의 조건에 모두 해당하는 경우의 수가 있음 
+# BAA 같은 것들.. 이럴 때는 backtracking하면 되나?
+# 그런데 재귀를 쓰기는 싫음
+
+# backtracking을 했어야 했어.. 근데 backtracking 없는 재귀로 되는 이유는 무엇일까? 
+# backtracking을 반복문으로 바꾸려면? stack + copy를 사용해 원본에 손상이 가지 않게 
