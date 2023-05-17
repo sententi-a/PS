fdb8c7e4f1f4967f56fa959608d97e4c3ad49866
HyejiYeom <kristi8041@gmail.com>
2023-05-17 14:34:54 +0900

    [Python3][SlidingWindow] 20437 문자열 게임 2

--- Baekjoon/ses9928/20437_문자열_게임_2.py
+++ Baekjoon/ses9928/20437_문자열_게임_2.py
@@ -9,17 +9,48 @@
 """
 
 import sys
+from collections import defaultdict, Counter
 
 for _ in range(int(sys.stdin.readline())):
-    string = sys.stdin.readline().rstrip()
-    limit = int(sys.stdin.readline())
+    string = sys.stdin.readline().rstrip() # 문자열 W 
+    limit = int(sys.stdin.readline()) # 정수 K 
 
-    start, end = 0, 0
+    # 정수 k가 1이거나, 문자열 w의 길이가 1이면 답은 무조건 1, 1
+    if limit == 1 or len(string) == 1:
+        print(1, 1)
+        continue
 
-    scs = 0 # 가장 짧은 연속 문자열
+    scs = 10**4 # 가장 짧은 연속 문자열 (이것도 가장 앞, 뒤 문자가 해당 문자일 때)
     lcs = 0 # 가장 긴 연속 문자열 
 
+    candidates = set()
+    counter = Counter(string) # 문자열 내에 각 문자가 몇 번 등장했는지 
+    indexes = defaultdict(list) # 문자열 내 각 문자가 어디에 위치하는지 
 
-    while start <= end and end < len(string)-1:
-        # 투포인터 사용
-        pass
+    # 문자열을 순회하면서 각 문자가 나타난 인덱스를 리스트로 저장
+    # 이 때, 특정 문자의 등장 횟수가 K번 이상이면 해당 문자를 시작과 끝으로 하는 substring 후보가 됨
+    for i in range(len(string)):
+        indexes[string[i]].append(i)
+        
+        if counter[string[i]] >= limit:
+            candidates.add(string[i])
+
+    # 만족하는 연속 문자열이 없을 때 (모든 문자의 개수가 k 이상이 아닐 때)
+    if not candidates:
+        print(-1)
+        continue
+    
+    # scs, lcs의 길이 갱신 (해당 문자가 딱 K번 포함되는 substring의 길이를 모두 구함)
+    for char in candidates:
+        if counter[char] == limit:
+            length = indexes[char][-1] - indexes[char][0] + 1
+            scs = min(scs, length)
+            lcs = max(lcs, length)
+        else:
+            for i in range(counter[char] - limit + 1):
+                length = indexes[char][i + limit - 1] - indexes[char][i] + 1
+                scs = min(scs, length)
+                lcs = max(lcs, length)
+
+    print(scs, lcs)
+            
