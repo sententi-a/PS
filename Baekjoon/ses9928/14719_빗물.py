63654cc0e6bccf7dcb99e093b24c9df2390134bf
HyejiYeom <kristi8041@gmail.com>
2023-05-25 01:22:39 +0900

    [Python3][Simulation] 14719 빗물
    예제는 잘 나오는데, 채점 돌려보면 바로 틀렸습니다 나옴

--- Baekjoon/ses9928/14719_빗물.py
+++ Baekjoon/ses9928/14719_빗물.py
@@ -11,31 +11,51 @@ import sys
 height, width = map(int, sys.stdin.readline().split())
 buildings = list(map(int, sys.stdin.readline().split()))
 
-# rain = 0
-rain = height * width - buildings[0]
-max_height, idx = buildings[0], 0
+rain = 0
+# rain = height * width - buildings[0]
+# max_height, idx = buildings[0], 0
 
-for i in range(1, width):
-    rain -= buildings[i]
+# for i in range(1, width):
+#     rain -= buildings[i]
 
-    if max_height <= buildings[i]:
-        empty = (height - max_height) * (i - idx)
-        rain -= empty
+#     if max_height <= buildings[i]:
+#         empty = (height - max_height) * (i - idx)
+#         rain -= empty
 
-        max_height, idx = buildings[i], i
-    # 가운데에 멈춰야 할 경우 어떻게 할지 생각해보기 
-    elif i == width - 1:
-        if buildings[i] > 0:
-            empty = (height - buildings[i]) * (i - idx)
-            rain -= empty
-        else:
-            rain -= height * (i - idx + 1)
+#         max_height, idx = buildings[i], i
+#     # 가운데에 멈춰야 할 경우 어떻게 할지 생각해보기 
+#     elif i == width - 1:
+#         if buildings[i] > 0:
+#             empty = (height - buildings[i]) * (i - idx)
+#             rain -= empty
+#         else:
+#             rain -= height * (i - idx + 1)
 
-    # print(rain, i)
+if width <= 2:
+    print(0)
+    exit()
+
+# left, right를 계속해서 바꿔가기
+left, right = 0, 0
+
+while left < width-1:
+    right = buildings.index(max(buildings[left+1:]), left+1)
+    # print(left, right)
+
+    criteria = min(buildings[left], buildings[right]) 
+
+    for i in range(left+1, right):
+        if buildings[i] < criteria:
+            rain += criteria - buildings[i]
+
+    left = right
+
 
     
-if rain < 0:
-    print(0)
+# if rain < 0:
+#     print(0)
 
-else:
-    print(rain)
+# else:
+#     print(rain)
+
+print(rain)
