423674d503302bc9ede96fd3e31a1aa1dd97c841
HyejiYeom <kristi8041@gmail.com>
2023-05-25 15:22:06 +0900

    [Python3][Simulation] 14719 빗물

--- Baekjoon/ses9928/14719_빗물.py
+++ Baekjoon/ses9928/14719_빗물.py
@@ -3,7 +3,6 @@
 """
 2차원 세계에 블록이 쌓여 있고, 비가 오면 블록 사이에 빗물이 고임
 비가 충분히 많이 온다고 할 때, 고이는 빗물의 총량은 얼마? 
-
 """
 
 import sys 
@@ -12,50 +11,44 @@ height, width = map(int, sys.stdin.readline().split())
 buildings = list(map(int, sys.stdin.readline().split()))
 
 rain = 0
-# rain = height * width - buildings[0]
-# max_height, idx = buildings[0], 0
-
-# for i in range(1, width):
-#     rain -= buildings[i]
-
-#     if max_height <= buildings[i]:
-#         empty = (height - max_height) * (i - idx)
-#         rain -= empty
-
-#         max_height, idx = buildings[i], i
-#     # 가운데에 멈춰야 할 경우 어떻게 할지 생각해보기 
-#     elif i == width - 1:
-#         if buildings[i] > 0:
-#             empty = (height - buildings[i]) * (i - idx)
-#             rain -= empty
-#         else:
-#             rain -= height * (i - idx + 1)
 
 if width <= 2:
     print(0)
     exit()
 
+def find_right_wall(left: int, end: int, buildings: list):
+    right = left + 1
+    # max_in_range = max(buildings[right:])
+
+    for i in range(right, end):
+        # left 건물보다 right 건물의 높이가 크거나 같을 때 
+        if buildings[left] <= buildings[i]:
+            return i
+        # left 건물보다 right 건물의 높이는 낮다면, 그 중에서도 가장 높은 건물을 찾아야 함
+        if buildings[right] < buildings[i]:
+            right = i
+    
+    return right
+
+def calculate_rain_amount(left: int, right: int, buildings: list, criteria: int):
+    cnt = 0 
+
+    for i in range(left+1, right):
+        cnt += criteria - buildings[i]
+    
+    return cnt
+
 # left, right를 계속해서 바꿔가기
 left, right = 0, 0
 
 while left < width-1:
-    right = buildings.index(max(buildings[left+1:]), left+1)
-    # print(left, right)
+    right = find_right_wall(left, width, buildings)
 
     criteria = min(buildings[left], buildings[right]) 
 
-    for i in range(left+1, right):
-        if buildings[i] < criteria:
-            rain += criteria - buildings[i]
+    rain += calculate_rain_amount(left, right, buildings, criteria)
 
     left = right
 
-
-    
-# if rain < 0:
-#     print(0)
-
-# else:
-#     print(rain)
 
 print(rain)
